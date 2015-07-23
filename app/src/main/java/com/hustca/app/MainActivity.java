9f3389a29cc722e33ceb1c3057e0eeea07ef0fd0
Hamster Tian <haotia@gmail.com>
2015-07-23 17:12:59 +0800

    NavigationView prototype, needs to be replaced
    by custom NavigationView since we need buttons at bottom.

--- app/src/main/java/com/hustca/app/MainActivity.java
+++ app/src/main/java/com/hustca/app/MainActivity.java
@@ -1,17 +1,22 @@
 package com.hustca.app;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar;
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
