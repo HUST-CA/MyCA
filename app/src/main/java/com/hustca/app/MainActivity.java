bc902247a2e53520051e452672fe54f118efb634
Hamster Tian <haotia@gmail.com>
2015-08-01 20:35:41 +0800

    Implement listener for navigation menu in drawer

--- app/src/main/java/com/hustca/app/MainActivity.java
+++ app/src/main/java/com/hustca/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.hustca.app;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,10 +11,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.hustca.app.fragments.HistoryFragment;
+import com.hustca.app.fragments.NewsFragment;
 import com.hustca.app.fragments.RecentActivitiesFragment;
 
 
 public class MainActivity extends AppCompatActivity implements RefreshIndicator {
+
 
     private static final String LOG_TAG = "MyCA_MA";
     SwipeRefreshLayout mSwipeRefreshLayout;
@@ -40,14 +44,47 @@ public class MainActivity extends AppCompatActivity implements RefreshIndicator 
             }
         });
 
+        setNavigationMenuListener();
     }
 
-    private void switchToFragment(Fragment newFragment) {
+    private void switchToFragment(FragmentType fragmentType) {
+        // Don't reload/new the same fragment
+        Fragment newFragment;
+        switch (fragmentType) {
+            case FRAGMENT_RECENT_ACTIVITIES:
+                if (mCurrentFragment instanceof RecentActivitiesFragment)
+                    return;
+                else
+                    newFragment = new RecentActivitiesFragment();
+                break;
+            case FRAGMENT_HISTORY:
+                if (mCurrentFragment instanceof HistoryFragment)
+                    return;
+                else
+                    newFragment = new HistoryFragment();
+                break;
+            case FRAGMENT_NEWS:
+                if (mCurrentFragment instanceof NewsFragment)
+                    return;
+                else
+                    newFragment = new NewsFragment();
+                break;
+            case FRAGMENT_H2O:
+                // TODO Implement this frag
+                // break;
+                return;
+            default:
+                Log.wtf(LOG_TAG, "switchToFragment: unknown frag type: " + fragmentType);
+                return;
+        }
+
+        // Do the real transaction
+        onRefreshStopped(); // Stop any refreshing indicator
         FragmentTransaction ft = this.getFragmentManager().beginTransaction();
         if (mCurrentFragment != null)
-            ft.replace(R.id.swipe_to_refresh_container, newFragment);
+            ft.replace(R.id.fragment_container, newFragment);
         else
-            ft.add(R.id.swipe_to_refresh_container, newFragment);
+            ft.add(R.id.fragment_container, newFragment);
         ft.commit();
         mCurrentFragment = newFragment;
     }
@@ -55,7 +92,8 @@ public class MainActivity extends AppCompatActivity implements RefreshIndicator 
     @Override
     public void onResume() {
         super.onResume();
-        switchToFragment(new RecentActivitiesFragment());
+        // Default fragment to show on boot
+        switchToFragment(FragmentType.FRAGMENT_RECENT_ACTIVITIES);
     }
 
     @Override
@@ -90,5 +128,61 @@ public class MainActivity extends AppCompatActivity implements RefreshIndicator 
     public void onRefreshStarted() {
         if (mSwipeRefreshLayout != null)
             mSwipeRefreshLayout.setRefreshing(true);
+    }
+
+    private void setNavigationMenuListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                FragmentType fragToSwitch;
+                switch (id) {
+                    case R.id.menu_recent_act:
+                        fragToSwitch = FragmentType.FRAGMENT_RECENT_ACTIVITIES;
+                        break;
+                    case R.id.menu_history:
+                        fragToSwitch = FragmentType.FRAGMENT_HISTORY;
+                        break;
+                    case R.id.menu_news:
+                        fragToSwitch = FragmentType.FRAGMENT_NEWS;
+                        break;
+                    case R.id.menu_h2o:
+                        fragToSwitch = FragmentType.FRAGMENT_H2O;
+                        break;
+                    /* Following can't be handled with FragmentType */
+                    case R.id.menu_settings:
+                        // TODO Settings activity
+                        menuItem.setChecked(true);
+                        return true;
+                    case R.id.menu_about:
+                        // TODO About
+                        menuItem.setChecked(true);
+                        return true;
+                    case R.id.menu_switch_org:
+                        // TODO
+                        menuItem.setChecked(true);
+                        return true;
+                    default:
+                        Log.wtf(LOG_TAG, "drawerMenu: unknown menu: " + menuItem.toString());
+                        return true;
+                }
+                switchToFragment(fragToSwitch);
+                menuItem.setChecked(true);
+                return true;
+            }
+        });
+    }
+
+    /**
+     * An enum for identifying fragments by a single int.
+     * This seems a little dirty but I can't think of better ways.
+     * Meant for switchToFragment without using /new Fragment()/ as param
+     */
+    private enum FragmentType {
+        FRAGMENT_RECENT_ACTIVITIES,
+        FRAGMENT_NEWS,
+        FRAGMENT_HISTORY,
+        FRAGMENT_H2O
     }
 }
