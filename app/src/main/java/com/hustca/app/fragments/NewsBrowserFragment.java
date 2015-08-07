3000c088fdfce75f5d114e67f43cb986297b507d
Hamster Tian <haotia@gmail.com>
2015-08-07 22:46:42 +0800

    Initial news browser/detail fragment

--- app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
+++ app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
@@ -1,6 +1,10 @@
 package com.hustca.app.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Hamster on 2015/8/2.
@@ -8,4 +12,9 @@ import android.app.Fragment;
  * A fragment displaying news/history detail.
  */
 public class NewsBrowserFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
