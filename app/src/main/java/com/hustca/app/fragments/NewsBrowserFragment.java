5e5756d981ae36f459264e7d89d10841adc253ed
Hamster Tian <haotia@gmail.com>
2015-08-11 21:33:00 +0800

    Remove redundant title text view in news browser
    CollapsingToolbarLayout itself can display this. My bad.

--- app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
+++ app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
@@ -2,6 +2,7 @@ package com.hustca.app.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,14 +27,16 @@ public class NewsBrowserFragment extends Fragment {
     public static final String KEY_ARTICLE_BUNDLE = "article";
 
     private static final String LOG_TAG = "MyCA_NewsBrowserFrag";
-    private TextView mHeaderTextView;
     private ImageView mHeaderImageView;
+    private TextView mDetailTextView;
+    private CollapsingToolbarLayout mCollapsingToolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.news_browser, container, false);
         mHeaderImageView = (ImageView) v.findViewById(R.id.news_browser_header_image);
-        mHeaderTextView = (TextView) v.findViewById(R.id.news_browser_header_text);
+        mCollapsingToolbar = (CollapsingToolbarLayout) v.findViewById(R.id.collapsing_toolbar);
+        mDetailTextView = (TextView) v.findViewById(R.id.news_browser_detail_text);
         return v;
     }
 
@@ -51,6 +54,12 @@ public class NewsBrowserFragment extends Fragment {
         AsyncImageGetter asyncImageGetter = new AsyncImageGetter(mHeaderImageView);
         asyncImageGetter.loadForImageView(article.getCoverURL());
 
-        mHeaderTextView.setText(article.getTitle());
+        mCollapsingToolbar.setTitle(article.getTitle());
+        // TODO Do we need to change the colors according to picture?
+        mCollapsingToolbar.setCollapsedTitleTextAppearance(R.style.TextAppearance_CollapsedCollapsingBar);
+        mCollapsingToolbar.setExpandedTitleTextAppearance(R.style.TextAppearance_ExpandedCollapsingBar);
+
+        // TODO
+        mDetailTextView.setText("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 }
