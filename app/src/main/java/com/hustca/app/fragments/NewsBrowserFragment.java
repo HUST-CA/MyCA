40275c4d2abb72baa5d3fd68fc9718f1d376c1df
Hamster Tian <haotia@gmail.com>
2015-08-11 20:08:47 +0800

    Load title and cover in news browser

--- app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
+++ app/src/main/java/com/hustca/app/fragments/NewsBrowserFragment.java
@@ -2,11 +2,16 @@ package com.hustca.app.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hustca.app.Article;
 import com.hustca.app.R;
+import com.hustca.app.util.AsyncImageGetter;
 
 /**
  * Created by Hamster on 2015/8/2.
@@ -20,15 +25,33 @@ public class NewsBrowserFragment extends Fragment {
      */
     public static final String KEY_ARTICLE_BUNDLE = "article";
 
+    private static final String LOG_TAG = "MyCA_NewsBrowserFrag";
+    private TextView mHeaderTextView;
+    private ImageView mHeaderImageView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.news_browser, container, false);
+        View v = inflater.inflate(R.layout.news_browser, container, false);
+        mHeaderImageView = (ImageView) v.findViewById(R.id.news_browser_header_image);
+        mHeaderTextView = (TextView) v.findViewById(R.id.news_browser_header_text);
+        return v;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        // TODO do the getArgument() things here.
+
+        Article article = getArguments().getParcelable(KEY_ARTICLE_BUNDLE);
+        if (article == null) {
+            Log.w(LOG_TAG, "onResume: article from Parcelable is null. Returning.");
+            // TODO notify user about broken object
+            return;
+        }
+
+        AsyncImageGetter asyncImageGetter = new AsyncImageGetter(mHeaderImageView);
+        asyncImageGetter.loadForImageView(article.getCoverURL());
+
+        mHeaderTextView.setText(article.getTitle());
     }
 }
