777525dc865cd536c24c132ebc583621c5e60b44
Hamster Tian <haotia@gmail.com>
2015-09-02 11:05:15 +0800

    Yes I HATE push services
    I WANT THEM GONE!

--- app/src/main/java/com/hustca/app/fragments/SettingsFragment.java
+++ app/src/main/java/com/hustca/app/fragments/SettingsFragment.java
@@ -1,10 +1,14 @@
 package com.hustca.app.fragments;
 
+import android.app.NotificationManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
+import com.hustca.app.PMReceiver;
 import com.hustca.app.R;
 
 import cn.jpush.android.api.JPushInterface;
@@ -34,16 +38,51 @@ public class SettingsFragment extends PreferenceFragment {
         mPrefPushEnabled.setOnPreferenceChangeListener(
                 new Preference.OnPreferenceChangeListener() {
                     @Override
-                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    public boolean onPreferenceChange(Preference preference, final Object newValue) {
+                        // This interface can't be called from another thread (Handler)
                         if ((boolean) newValue) {
                             JPushInterface.resumePush(mContext);
                         } else {
                             JPushInterface.stopPush(mContext);
+
+                            // Clear all notifications, especially those "Missing stats code"
+                            NotificationManager nm = (NotificationManager)
+                                    mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                            nm.cancelAll();
                         }
+
+                        // Disable services and receivers as well - I have OBD
+                        // And this is a bit time-consuming, do it in another thread
+                        new android.os.Handler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                setPushServicesEnabled((boolean) newValue);
+                            }
+                        });
                         return true;
                     }
                 });
-
     }
 
+    private void setPushServicesEnabled(boolean doYouLikeMe) {
+        PackageManager pm = mContext.getPackageManager();
+        ComponentName pushServices[] = new ComponentName[]{
+                new ComponentName(mContext, PMReceiver.class),
+                new ComponentName(mContext, "cn.jpush.android.service.PushService"),
+                new ComponentName(mContext, "cn.jpush.android.service.DaemonService"),
+                new ComponentName(mContext, "cn.jpush.android.service.PushReceiver"),
+                new ComponentName(mContext, "cn.jpush.android.ui.PushActivity"),
+                new ComponentName(mContext, "cn.jpush.android.service.DownloadService")
+        };
+
+        int state = doYouLikeMe ?
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+
+        for (ComponentName service : pushServices) {
+            pm.setComponentEnabledSetting(service,
+                    state,
+                    PackageManager.DONT_KILL_APP);
+        }
+    }
 }
