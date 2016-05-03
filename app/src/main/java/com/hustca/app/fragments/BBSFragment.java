72f4edb57fb417cd3efdf21754ddd2150b9b31fe
Hamster Tian <haotia@gmail.com>
2016-05-03 22:42:12 +0800

    WebView changes: 3r-pty cookies, base URL
    * Accepts 3rd-party cookies for logging into Discuz
    * Opens URL containing the "base" URL within the app, otherwise launch an external browser.

--- app/src/main/java/com/hustca/app/fragments/BBSFragment.java
+++ app/src/main/java/com/hustca/app/fragments/BBSFragment.java
@@ -14,10 +14,16 @@ import android.widget.FrameLayout;
  */
 public class BBSFragment extends PlainBrowserFragment {
     private static final String URL = "http://wsq.discuz.qq.com/?c=index&a=index&f=wx&fid=2&siteid=265482436";
+    private static final String BASE_URL = "wsq.discuz.qq.com";
 
     @Override
     protected String getURL() {
         return URL;
+    }
+
+    @Override
+    protected String getBaseURL() {
+        return BASE_URL;
     }
 
     @Nullable
--- app/src/main/java/com/hustca/app/fragments/PlainBrowserFragment.java
+++ app/src/main/java/com/hustca/app/fragments/PlainBrowserFragment.java
@@ -1,11 +1,15 @@
 package com.hustca.app.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -25,6 +29,18 @@ public class PlainBrowserFragment extends Fragment {
      * URL to load, passed as an argument in a bundle.
      */
     public static final String KEY_URL = "url";
+    /**
+     * Links under this base URL will be opened within the app.
+     * Otherwise links will be opened by external browsers.
+     * Since the base URL is searched in the whole URL (not just comparing from beginning)
+     * so it can be a part of the URL.
+     * <p/>
+     * Passed as an argument.
+     */
+    public static final String KEY_BASE_URL = "base_url";
+
+    private static final String DEFAULT_BASE_URL = "www.hustca.com/hustca";
+    private String mBaseUrl;
 
     protected WebView mWebView;
     protected ProgressBar mProgressBar;
@@ -42,6 +58,18 @@ public class PlainBrowserFragment extends Fragment {
         return getArguments().getString(KEY_URL, "about:blank");
     }
 
+    /**
+     * How to determine the base URL.
+     * <p/>
+     * By default, this method reads from the arguments and find
+     * a key named "base_url".
+     *
+     * @return Base URL that need to be opened within app
+     */
+    protected String getBaseURL() {
+        return getArguments().getString(KEY_BASE_URL, DEFAULT_BASE_URL);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -55,14 +83,28 @@ public class PlainBrowserFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
+        mBaseUrl = getBaseURL();
+
         mProgressBar.setMax(100); // newProgress documentation. For later use.
         mProgressBar.setIndeterminate(true);
+
+        /* For logging into Discuz */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
 
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+                if (url.indexOf(mBaseUrl) > 0) {
+                    view.loadUrl(url);
+                    return true;
+                } else {
+                    /* Simply call super.thisMethod won't let you choose browsers */
+                    Uri uri = Uri.parse(url);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                    return true;
+                }
             }
         });
         mWebView.setWebChromeClient(new WebChromeClient() {
@@ -81,11 +123,7 @@ public class PlainBrowserFragment extends Fragment {
         webSettings.setJavaScriptEnabled(true);
 
         /* Enable LocalStorage, for Discuz wsg.qq.com */
-        mWebView.getSettings().setDomStorageEnabled(true);
-        String appCachePath = getActivity().getCacheDir().getAbsolutePath();
-        mWebView.getSettings().setAppCachePath(appCachePath);
-        mWebView.getSettings().setAllowFileAccess(true);
-        mWebView.getSettings().setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
 
         mProgressBar.setVisibility(View.VISIBLE);
         mWebView.loadUrl(getURL());
@@ -94,6 +132,4 @@ public class PlainBrowserFragment extends Fragment {
         after 1st update (we got percentage) show the progress clearly.
          */
     }
-
-
 }
